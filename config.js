165911a2944aa407fbf5c32946bd8339aa536074
givemenamefr <138551882+givemenamefr@users.noreply.github.com>
2026-02-08 14:20:17 +0800

    Update config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ const CONFIG = {
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
         hearts: ['❤️', '💖', '💝', '💗', '💓' , '🪻' ],  // Heart emojis
-        bears: ['🧸', '🐻', '🎀', '🌷', '🌸' '🌺', '🏵️' , '🌹'],    // Cute bear emojis
+        bears: ['🧸', '🐻', '🎀', '🌷', '🌸' '🌺', '🏵️' , '🌹']    // Cute bear emojis
     },
 
     // Questions and answers
